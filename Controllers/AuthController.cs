5343eb9c6daf08ca281c45cae3a0770e1c917077
Laddaporn H. <laddaporn@bangchak.co.th>
2024-05-28 14:08:21 +0700

    release v 2.2.0

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -36,7 +36,7 @@ public class AuthController : ControllerBase
     [HttpGet]
     public IActionResult Home()
     {
-        return Ok(new { message = "Hello Auth service 2.1.0" });
+        return Ok(new { message = "Hello Auth service 2.2.0" });
     }
 
     // localhost:port/api/v1/Auth/Register
